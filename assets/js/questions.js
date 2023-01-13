90920dd63c8ec2dcb96498315977c857fdce7aae
Spacepineapple <neal.t.reeves@gmail.com>
2023-01-13 18:28:54 +0000

    Add a nested question object with 5 questions to questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,42 @@
+let questions = {
+    question1: {
+        question: "Which of the Following is not a JavaScript Primative?",
+        answerA: "String",
+        answerB: "Boolean",
+        answerC: "Dictionary",
+        answerD: "Null",
+        correct: "Dictionary"
+    },
+    question2: {
+        question: "Which of these Are Hoisted?",
+        answerA: "Arrow Function",
+        answerB: "Function Expressions",
+        answerC: "Anonymous Functions",
+        answerD: "Named Functions",
+        correct: "Named Functions"
+    },
+    question3: {
+        question: "What Does 'this' Refer to in the Browser's Global Scope?",
+        answerA: "The Window Object",
+        answerB: "Local Storage",
+        answerC: "The Computer",
+        answerD: "The Console",
+        correct: "The Window Object"
+    },
+    question4: {
+        question: "Which of the Following Defines an Immutable Variable?",
+        answerA: "Let",
+        answerB: "Const",
+        answerC: "Var",
+        answerD: "Def",
+        correct: "Const"
+    },
+    question5: {
+        question: "What is a Method?",
+        answerA: "An Object Property Function",
+        answerB: "An Algorithm",
+        answerC: "A Type of Loop",
+        answerD: "A Data Type"
+
+    }
+}
