e9ca186d121196d2f881ea688eaa21ce5a16a629
Spacepineapple <neal.t.reeves@gmail.com>
2023-01-16 21:00:33 +0000

    Add function to clear local storage and remove second user in score display

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,11 +1,19 @@
 let highScores = JSON.parse(localStorage.getItem("highScores"));
 console.log(highScores);
 let initials = Object.keys(highScores);
-let scores = Object.keys(highScores);
+let scores = Object.values(highScores);
 let scoreArea = document.getElementById("highscores");
+let clearButton = document.getElementById("clear");
 
 for (let i=0; i<scores.length; i++) {
     const li = document.createElement("li");
     li.textContent = `${initials[i]} - ${scores[i]}`;
     scoreArea.appendChild(li);
 }
+
+clearButton.addEventListener("click", clearStorage);
+
+function clearStorage() {
+    localStorage.clear();
+}
+
